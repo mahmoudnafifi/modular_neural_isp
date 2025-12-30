d7f08e15251b312332b8de96b914d4e27d8dd943
Mahmoud Afifi <37669469+mahmoudnafifi@users.noreply.github.com>
2025-12-30 13:06:57 -0500

    v0.1.5 (Beta)
    v0.1.5 (Beta): Added support for Apple Silicon (MPS) during raw-JPEG parameter embedding.

--- utils/constants.py
+++ utils/constants.py
@@ -37,7 +37,7 @@ elif os_name == 'Darwin':
 else:
   EXIFTOOL_PATH = '/usr/bin/exiftool'  # Update accordingly
 
-VERSION = 'v0.1.4-beta'
+VERSION = 'v0.1.5-beta'
 
 EPS = 0.00000001
 
@@ -287,6 +287,7 @@ ABOUT_MESSAGE = (
     '• Raghav Goyal – denoiser experiments\n\n'
     'Inquiries: Mahmoud Afifi (m.3afifi@gmail.com)'
 )
+
 
 
 
